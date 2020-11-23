f610cc36f1ae07076069a0881b67158efcf71cce
Huemac-Alfredo <hmca101292@outlook.com>
2020-11-23 14:05:11 -0600

    get users info

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-puts "Hello world"
+#To get players information
+puts "Please insert the name of player A"
+player_a = gets.chomp
+puts "Player A uses symbol X"
+puts "Please insert the name of player B"
+player_b = gets.chomp
+puts "Player A uses symbol O"
