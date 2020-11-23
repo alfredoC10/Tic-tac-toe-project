ec6d2b5320f0d21dc6663a3b1942b1c9ca87ebf8
Huemac-Alfredo <hmca101292@outlook.com>
2020-11-23 16:40:33 -0600

    Set game flow

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,4 @@
-# !/usr/bin/env ruby
+#!/usr/bin/env ruby
 # rubocop:disable Lint/UselessAssignment
 # To get players information
 puts 'Please insert the name of player A'
@@ -8,8 +8,51 @@ puts 'Player A uses symbol: X'
 puts 'Please insert the name of player B'
 player_b = gets.chomp
 puts 'Player A uses symbol: O'
-
-puts 'Please write your symbol in the available spaces'
+puts 'Player A, is your turn, write "X" in the available spaces'
+#             |              |
+#       _     |       _      |     _
+# ___________1|_____________2|____________3
+#             |              |
+#       _     |       _      |     _
+# ___________4|_____________5|____________6
+#             |              |
+#       _     |       _      |     _
+#            7|             8|            9
+puts 'Your move is not valid, please choose another available space'
+puts 'Your move is valid, please choose another available space'
+#             |              |
+#       X     |       _      |     _
+# ___________1|_____________2|____________3
+#             |              |
+#       _     |       _      |     _
+# ___________4|_____________5|____________6
+#             |              |
+#       _     |       _      |     _
+#            7|             8|            9
+puts 'Player B, is your turn, write "O" in the available spaces'
+#             |              |
+#       X     |       _      |     _
+# ___________1|_____________2|____________3
+#             |              |
+#       O     |       _      |     _
+# ___________4|_____________5|____________6
+#             |              |
+#       _     |       _      |     _
+#            7|             8|            9
+puts 'Your move is not valid, please choose another available space'
+puts 'Your move is valid, please choose another available space'
+#             |              |
+#       X     |       X      |     X
+# ___________1|_____________2|____________3
+#             |              |
+#       O     |       O      |     _
+# ___________4|_____________5|____________6
+#             |              |
+#       _     |       _      |     _
+#            7|             8|            9
+puts 'Player A won!'
+puts 'Would you like to play again? Y/N'
+new_game = gets.chomp
 
 #             |              |
 #       _     |       _      |     _
@@ -20,10 +63,6 @@ puts 'Please write your symbol in the available spaces'
 #             |              |
 #       _     |       _      |     _
 #            7|             8|            9
-#
-#
-#       winner lines with the same symbol
-#       1-2-3      1-4-7     1-5-9
-#       4-5-6      2-5-8     3-5-7
-#       7-8-9      3-6-9
+puts 'You selected not to play again, thanks for playing!'
+
 # rubocop:enable Lint/UselessAssignment
