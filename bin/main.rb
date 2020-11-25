3f8112939af87fe8cf62e4eb716072d205ed8427
cgcarlosg <cgcarlosg@hotmail.com>
2020-11-25 09:14:20 -0500

    add chmod +

--- bin/main.rb
+++ bin/main.rb
@@ -30,7 +30,7 @@ def handle_turn
   replay = gets.chomp.upcase
   case replay
   when 'Y'
-    puts "\nLet's play again"
+    puts "\nLet's play again "
     board = %w[1 2 3 4 5 6 7 8 9]
     puts "#{board[0]} | #{board[1]} | #{board[2]}"
     puts "#{board[3]} | #{board[4]} | #{board[5]}"
