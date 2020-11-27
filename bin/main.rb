759f3da19e3538eb8f6f43b62b8f11fec960a257
cgcarlosg <cgcarlosg@hotmail.com>
2020-11-27 11:16:46 -0500

    fix linters

--- bin/main.rb
+++ bin/main.rb
@@ -3,24 +3,18 @@ require './lib/gametictac'
 
 tctc = Tictactoe.new
 
-puts tctc.display
+puts "\nLet's start to play"
 
-    puts "\nLet's start to play"
-
-
-  until  tctc.win? == 'X won!' || tctc.win? == 'O won!' || tctc.full? == true   do
-    puts "\nPlease select a position from 1 to 9, ('X' starts and 'O' continues)\n"
-    position = gets.chomp.to_i
-    if tctc.valid_movements?(position)
-      tctc.move(position)
-      puts tctc.display
-    else
-      puts "Invalid movement"
-      puts "\nPlease select a position from 1 to 9\n"
-      puts tctc.display
-    end
+until tctc.win? == 'X won!' || tctc.win? == 'O won!' || tctc.full? == true
+  puts tctc.display
+  puts "\nPlease select a position from 1 to 9, ('X' starts and 'O' continues)\n"
+  position = gets.chomp.to_i
+  if tctc.valid_movements?(position)
+    tctc.move(position)
+  else
+    puts 'Invalid movement'
+    puts "\nPlease select a position from 1 to 9\n"
   end
+end
 
-  if tctc.win? == 'X won!' || tctc.win? == 'O won!' || tctc.full? == true 
-    puts tctc.win? + '  GAME OVER'
-  end
+puts "#{tctc.win?}  GAME OVER" if tctc.win? == 'X won!' || tctc.win? == 'O won!' || tctc.full? == true
--- lib/gametictac.rb
+++ lib/gametictac.rb
@@ -1,46 +1,47 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
 class Tictactoe
-    attr_reader :board
-    def initialize()
-        @symbol1 = 'X' 
-        @symbol2 = 'O'
-        @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-        @turn = true
-      end
+  attr_reader :board
 
-      def display
-        puts "        #{board[0]} | #{board[1]} | #{board[2]} "
-        puts "      -------------"
-        puts "        #{board[3]} | #{board[4]} | #{board[5]} "
-        puts "      -------------"
-        puts "        #{board[6]} | #{board[7]} | #{board[8]} "
-      end
+  def initialize()
+    @symbol1 = 'X'
+    @symbol2 = 'O'
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @turn = true
+  end
 
-      def valid_movements?(position)
-        (position > 0 && position < 10) && board[position.to_i-1] == " "
-      end
+  def display
+    puts "        #{board[0]} | #{board[1]} | #{board[2]} "
+    puts '      -------------'
+    puts "        #{board[3]} | #{board[4]} | #{board[5]} "
+    puts '      -------------'
+    puts "        #{board[6]} | #{board[7]} | #{board[8]} "
+  end
 
-      def move(position)
-        board[position.to_i-1] = @turn ? @symbol1 : @symbol2
-        @turn = !@turn
-      end
+  def valid_movements?(position)
+    (position.positive? && position < 10) && board[position.to_i - 1] == ' '
+  end
 
-      def full?
-      board.none? " "
-      end
+  def move(position)
+    board[position.to_i - 1] = @turn ? @symbol1 : @symbol2
+    @turn = !@turn
+  end
 
-      def win?
-        winner = ""
-        if board[0] == @symbol1 && board[1] == @symbol1 && board[2] == @symbol1 ||
-           board[3] == @symbol1 && board[4] == @symbol1 && board[5] == @symbol1 ||
-           board[6] == @symbol1 && board[7] == @symbol1 && board[8] == @symbol1 ||
-           board[0] == @symbol1 && board[3] == @symbol1 && board[6] == @symbol1 ||
-           board[1] == @symbol1 && board[4] == @symbol1 && board[7] == @symbol1 ||
-           board[2] == @symbol1 && board[5] == @symbol1 && board[8] == @symbol1 ||
-           board[0] == @symbol1 && board[4] == @symbol1 && board[8] == @symbol1 ||
-           board[2] == @symbol1 && board[4] == @symbol1 && board[6] == @symbol1
-           winner = 'X won!'
-        elsif
-          board[0] == @symbol2 && board[1] == @symbol2 && board[2] == @symbol2 ||
+  def full?
+    board.none? ' '
+  end
+
+  def win?
+    if board[0] == @symbol1 && board[1] == @symbol1 && board[2] == @symbol1 ||
+       board[3] == @symbol1 && board[4] == @symbol1 && board[5] == @symbol1 ||
+       board[6] == @symbol1 && board[7] == @symbol1 && board[8] == @symbol1 ||
+       board[0] == @symbol1 && board[3] == @symbol1 && board[6] == @symbol1 ||
+       board[1] == @symbol1 && board[4] == @symbol1 && board[7] == @symbol1 ||
+       board[2] == @symbol1 && board[5] == @symbol1 && board[8] == @symbol1 ||
+       board[0] == @symbol1 && board[4] == @symbol1 && board[8] == @symbol1 ||
+       board[2] == @symbol1 && board[4] == @symbol1 && board[6] == @symbol1
+      'X won!'
+    elsif board[0] == @symbol2 && board[1] == @symbol2 && board[2] == @symbol2 ||
           board[3] == @symbol2 && board[4] == @symbol2 && board[5] == @symbol2 ||
           board[6] == @symbol2 && board[7] == @symbol2 && board[8] == @symbol2 ||
           board[0] == @symbol2 && board[3] == @symbol2 && board[6] == @symbol2 ||
@@ -48,10 +49,11 @@ class Tictactoe
           board[2] == @symbol2 && board[5] == @symbol2 && board[8] == @symbol2 ||
           board[0] == @symbol2 && board[4] == @symbol2 && board[8] == @symbol2 ||
           board[2] == @symbol2 && board[4] == @symbol2 && board[6] == @symbol2
-          winner = 'O won!'
-        else
-          winner = 'No winner'
-        end
-        winner
-      end 
+      'O won!'
+    else
+      'No winner'
+    end
+  end
 end
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
