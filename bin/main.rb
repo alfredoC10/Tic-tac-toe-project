a9e6aab017f67dfd51bd9eac2ec84d5f86dbab8b
cgcarlosg <cgcarlosg@hotmail.com>
2020-11-27 21:49:45 -0500

    fixed according to last reviewer

--- bin/main.rb
+++ bin/main.rb
@@ -3,22 +3,24 @@ require './lib/gametictac'
 
 puts "Please select a symbol to play: 'X' / 'O'"
 symbol = gets.chomp.upcase
+until %w[X O].include?(symbol)
+  puts "Invalid Selection. Please select a symbol to play: 'X' / 'O'"
+  symbol = gets.chomp.upcase
+end
 
 tctc = Tictactoe.new(symbol)
-
 puts "\nLet's start to play"
 
-until tctc.win? == 'X won!' || tctc.win? == 'O won!' || tctc.full? == true
+until tctc.win? == 'X wins!' || tctc.win? == 'O wins!' || tctc.full? == true
   puts tctc.display
   puts "\nPlease select a position from 1 to 9\n"
   position = gets.chomp.to_i
   if tctc.valid_movements?(position)
     tctc.move(position)
+    puts "last symbol that was played: '#{tctc.win?}'"
   else
     puts 'Invalid movement'
     puts "\nPlease select a position from 1 to 9\n"
   end
 end
-# puts tctc.last_movement
-puts "#{tctc.win?}  GAME OVER" if tctc.win? == 'X won!' || tctc.win? == 'O won!' || tctc.full? == true
 puts tctc.display
--- lib/gametictac.rb
+++ lib/gametictac.rb
@@ -41,27 +41,16 @@ class Tictactoe
   end
 
   def win?
-    if board[0] == 'X' && board[1] == 'X' && board[2] == 'X' ||
-       board[3] == 'X' && board[4] == 'X' && board[5] == 'X' ||
-       board[6] == 'X' && board[7] == 'X' && board[8] == 'X' ||
-       board[0] == 'X' && board[3] == 'X' && board[6] == 'X' ||
-       board[1] == 'X' && board[4] == 'X' && board[7] == 'X' ||
-       board[2] == 'X' && board[5] == 'X' && board[8] == 'X' ||
-       board[0] == 'X' && board[4] == 'X' && board[8] == 'X' ||
-       board[2] == 'X' && board[4] == 'X' && board[6] == 'X'
-      'X won!'
-    elsif board[0] == 'O' && board[1] == 'O' && board[2] == 'O' ||
-          board[3] == 'O' && board[4] == 'O' && board[5] == 'O' ||
-          board[6] == 'O' && board[7] == 'O' && board[8] == 'O' ||
-          board[0] == 'O' && board[3] == 'O' && board[6] == 'O' ||
-          board[1] == 'O' && board[4] == 'O' && board[7] == 'O' ||
-          board[2] == 'O' && board[5] == 'O' && board[8] == 'O' ||
-          board[0] == 'O' && board[4] == 'O' && board[8] == 'O' ||
-          board[2] == 'O' && board[4] == 'O' && board[6] == 'O'
-      'O won!'
-    else
-      'No winner'
+    winner = !@turn ? @symbol1.symbol : @symbol2.symbol
+    if (board[0..2] || board[3..5] || board[6..8]).all?(winner) ||
+       [board[0], board[3], board[6]].all?(winner) ||
+       [board[1], board[4], board[7]].all?(winner) ||
+       [board[2], board[5], board[8]].all?(winner) ||
+       [board[0], board[4], board[8]].all?(winner) ||
+       [board[2], board[4], board[6]].all?(winner)
+      winner = "#{winner} wins!"
     end
+    winner
   end
 end
 # rubocop:enable Metrics/CyclomaticComplexity
